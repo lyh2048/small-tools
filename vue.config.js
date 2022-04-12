d1534a1efe5ab6db46517d3b85034baa837dfa94
lyh <1046818625@qq.com>
2022-04-12 20:12:19 +0800

    修改页面标题

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     publicPath: './',
     chainWebpack: config => {
         config.plugin('html').tap(args => {
-            args[0].title = '在线工具'
+            args[0].title = '在线工具 - 我的工具箱'
             return args
         })
     }
