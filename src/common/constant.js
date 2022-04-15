ea911e07e674b5a9af688a5250ee14b08f26bab8
lyh <1046818625@qq.com>
2022-04-15 18:25:02 +0800

    修改接口地址

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:9001/watermark/v1/watermark'
-export const UPLOAD_PATH = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:9001/watermark/v1/image/upload'
-export const BASE_WEBSOCKET_URL = process.env.NODE_ENV === 'production' ? '' : 'ws://localhost:9001/watermark/websocket'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://47.94.46.65:9001/watermark/v1/watermark' : 'http://localhost:9001/watermark/v1/watermark'
+export const UPLOAD_PATH = process.env.NODE_ENV === 'production' ? 'http://47.94.46.65:9001/watermark/v1/image/upload' : 'http://localhost:9001/watermark/v1/image/upload'
+export const BASE_WEBSOCKET_URL = process.env.NODE_ENV === 'production' ? 'ws://47.94.46.65:9001/watermark/websocket' : 'ws://localhost:9001/watermark/websocket'
